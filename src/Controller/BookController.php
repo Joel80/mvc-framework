2703c6f43c688d642b196a78a76ad786361cd7b6
Joel Löfgren <jdtlofgren@gmail.com>
2021-05-01 17:06:35 +0100

    ORM solution for book-page

---
+++ src/Controller/BookController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Book;
+
+class BookController extends AbstractController
+{
+    /**
+     * @Route("/book", name="book")
+     */
+    public function index(): Response
+    {
+        return $this->render('book/index.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+
+    /**
+     * @Route("/book/all", name="find_all_book")
+     */
+    public function findAllBook(EntityManagerInterface $entityManager): Response
+    {
+        $books = $entityManager
+            ->getRepository(Book::class)
+            ->findAll();
+        
+        return $this->render('book/find_all.html.twig', ['books' => $books]);
+    }
+}
